68bcccfae5c14f4aa5b2b64a66ac067b9c782f38
Алла Коваленко <alla.kovalenko.pm.2020@lpnu.ua>
2022-11-26 23:02:07 +0200

    first commit

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3f10444a-2e10-4e83-85ae-1402d02f67e8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using AnalaizerClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RunEstimateTest
+{
+    [TestClass]
+    public class AnalaizerClassTests
+    {
+        public TestContext TestContext { get; set; }
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+            @"C:\Users\Пользователь\Desktop\TEST\lab1\Calculator\Calculator\UnitTest\DataTests.xml",
+            "Test", 
+            DataAccessMethod.Sequential)]
+
+        [TestMethod]
+        public void RunEstimateTest()
+        {
+            string expected = Convert.ToString(TestContext.DataRow["expected"]);
+            AnalaizerClass.expression = Convert.ToString(TestContext.DataRow["initialazied"]);
+            string actual = AnalaizerClass.RunEstimate();
+            Assert.AreEqual(expected, actual);
+        } 
+        [TestMethod]
+        public void RunEst1()
+        {
+            string expected = "&";
+            AnalaizerClass.expression = "56+";
+            string actual = AnalaizerClass.RunEstimate();
+            Assert.AreEqual(expected, actual);
+        }[TestMethod]
+        public void RunEst2()
+        {
+            string expected = "17";
+            AnalaizerClass.expression = "(5+1)+6+5/(5+5-5)%2";
+            string actual = AnalaizerClass.RunEstimate();
+            Assert.AreEqual(expected, actual);
+        }[TestMethod]
+        public void RunEst3()
+        {
+            string expected = "&Error 08 — Сумарна кількість чисел і операторів перевищує 30.";
+            AnalaizerClass.expression = "2-1-2-1-2-1-2-3-1-1-2-2-1-1-2-1-12-1-12-1-2-1-3-1-2-1-2-1-2-1-2-1-2-1-2-1";
+            string actual = AnalaizerClass.RunEstimate();
+            Assert.AreEqual(expected, actual);
+        }[TestMethod]
+        public void RunEst4()
+        {
+            string expected = "&";
+            AnalaizerClass.expression = "8-3*";
+            string actual = AnalaizerClass.RunEstimate();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
